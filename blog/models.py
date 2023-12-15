40d58bbcd6079fc9c69c3ee7b9074a7214c43c97
asbilim <bilimdepaulilian@gmail.com>
2023-12-15 20:49:25 +0100

    i gave the opportunity to also upload the image from the admin dashboard , instead of putting only the link

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
 from cloudinary.models import CloudinaryField
-from django_ckeditor_5.fields import CKEditor5Field
+
+from ckeditor_uploader.fields import RichTextUploadingField
 
 User = get_user_model()
 
@@ -45,8 +46,8 @@ class Article(models.Model):
     image = models.ImageField(upload_to="blog-images/",null=True,blank=True)
     title = models.CharField(max_length=500)
     date = models.DateTimeField(auto_now_add=True)
-    intro = CKEditor5Field('intro', config_name='extends')
-    body = CKEditor5Field('body', config_name='extends')
+    intro = RichTextUploadingField('intro', config_name='extends')
+    body = RichTextUploadingField('body', config_name='extends')
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
     
 
--- skyback/settings.py
+++ skyback/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_ckeditor_5',
     'drf_yasg',
-    'blog'
+    'blog',
+    'ckeditor_uploader',
+    'ckeditor'
 ]
 
 
@@ -257,12 +259,68 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 ############################################### CKEDITOR #########################################
 
 CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'full',
-        'height': 300,
+    'extends': {
+        'skin': 'moono',
+        # 'skin': 'office2013',
+        'toolbar_Basic': [
+            ['Source', '-', 'Bold', 'Italic']
+        ],
+        'toolbar_YourCustomToolbarConfig': [
+            {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']},
+            {'name': 'clipboard', 'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+            {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
+            {'name': 'forms',
+             'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+                       'HiddenField']},
+            '/',
+            {'name': 'basicstyles',
+             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+            {'name': 'paragraph',
+             'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
+                       'Language']},
+            {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
+            {'name': 'insert',
+             'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
+            '/',
+            {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
+            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+            {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
+            {'name': 'about', 'items': ['About']},
+            '/',  # put this to force next toolbar on new line
+            {'name': 'yourcustomtools', 'items': [
+                # put the name of your editor.ui.addButton here
+                'Preview',
+                'Maximize',
+
+            ]},
+        ],
+        'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
+        # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
+        # 'height': 291,
+        # 'width': '100%',
+        # 'filebrowserWindowHeight': 725,
+        # 'filebrowserWindowWidth': 940,
+        # 'toolbarCanCollapse': True,
+        # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
+        'tabSpaces': 4,
         'extraPlugins': ','.join([
-            'uploadimage',  # enables image upload
-            # ... other plugins
+            'uploadimage', # the upload image feature
+            # your extra plugins here
+            # 'div',
+            # 'autolink',
+            # 'autoembed',
+            # 'embedsemantic',
+            # 'autogrow',
+            # # 'devtools',
+            # 'widget',
+            # 'lineutils',
+            # 'clipboard',
+            # 'dialog',
+            # 'dialogui',
+            # 'elementspath'
         ]),
-    },
+    }
 }
+
+CKEDITOR_ALLOW_NONIMAGE_FILES = False
